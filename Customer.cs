e917e47082581ad3bcc0bf283b569310ff7194a1
Justin Haldeman <jmanh14@gmail.com>
2020-02-18 07:30:42 -0600

    Added more names to my list of names

--- Customer.cs
+++ Customer.cs
@@ -19,7 +19,7 @@ namespace LemonadeStand_3DayStarter
         public string tastePreference;
         public Customer(Random rng)
         {
-            firstNames = new List<string>() { "Justin", "Eddie", "Matt", "Joey", "Tarik", "Braden", "Nick", "Osman", "Gavin", "Cody" };
+            firstNames = new List<string>() { "Justin", "Eddie", "Matt", "Joey", "Tarik", "Braden", "Nick", "Osman", "Gavin", "Cody", "James", "John", "Paula", "Jackson", "Brooke", "Jamie", "Jared", "Brad", "Tom", "Linda", "Doug"};
             lastNames = new List<string>() { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
             payPreferences = new List<double>() { .05, .1, .15, .2, .25, .3, .35, .4, .45, .5 , .5,.55, .6, .65, .7, .75, .8, .85, .9, .95, 1};
             tastePreferences = new List<string>() { "Sweet", "Sour", "Balanced" , "Bland", "Watery"};
